359dd2db3425e18a1d214ff46695897b419f00b6
macgroigar <stephenmacgregor4@gmail.com>
2022-08-16 21:18:29 +0100

    guideline notes

---
+++ main.py
@@ -0,0 +1,9 @@
+#1. Create a greeting for users when using program.
+
+#2. Ask the user for the city they grew up in.
+
+#3. Ask user name of a pet.
+
+#4. Combine the name of pet and city to show band name.
+
+#5. Make sure the input cursor shows on new line.
